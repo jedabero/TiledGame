bef80efa912e2234980ad3e62136481ea8edad87
jedabero <jedabero@hotmail.com>
2015-10-25 18:43:28 -0500

    First Commit

---
+++ src/com/jedabero/tiledgame/Launcher.java
@@ -0,0 +1,13 @@
+package com.jedabero.tiledgame;
+
+/**
+ * TiledGame com.jedabero.tiledgame.Launcher
+ * Created by jedabero on 25/10/15.
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.print("Hello TiledGame");
+    }
+
+}
