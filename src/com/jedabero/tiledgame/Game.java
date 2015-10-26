698dff8896c2385de0331b101982d9d1d3574fee
jedabero <jedabero@hotmail.com>
2015-10-25 19:16:44 -0500

    Added made Game runnable.

--- src/com/jedabero/tiledgame/Game.java
+++ src/com/jedabero/tiledgame/Game.java
@@ -6,19 +6,61 @@ import com.jedabero.tiledgame.display.Display;
  * Game
  * Created by jedabero on 25/10/15.
  */
-public class Game {
+public class Game implements Runnable {
 
     private Display display;
-
     private String title;
     private int width, height;
 
+    private boolean running = false;
+    private Thread thread;
+
     public Game (String title, int width, int height) {
+        this.title = title;
         this.width = width;
         this.height = height;
-        this.title = title;
+    }
 
+    private void init() {
         display = new Display(title, width, height);
     }
 
+    private void tick() {
+
+    }
+
+    private void render() {
+
+    }
+
+    @Override
+    public void run() {
+        init();
+
+        while (running) {
+            tick();
+            render();
+        }
+
+        stop();
+    }
+
+    public synchronized void start() {
+        if (running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        if (!running)
+            return;
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/jedabero/tiledgame/Launcher.java
+++ src/com/jedabero/tiledgame/Launcher.java
@@ -1,15 +1,14 @@
 package com.jedabero.tiledgame;
 
-import com.jedabero.tiledgame.display.Display;
-
 /**
- * TiledGame com.jedabero.tiledgame.Launcher
+ * TiledGame Launcher
  * Created by jedabero on 25/10/15.
  */
 public class Launcher {
 
     public static void main(String[] args) {
-        new Game("TiledGame", 300, 300);
+        Game game = new Game("TiledGame", 300, 300);
+        game.start();
     }
 
 }
